c5f2476d08a266a0384afb2ec995c7edb79e3c77
Lukas Pfahler <lukas.pfahler@udo.edu>
2024-09-26 11:03:31 +0000

    use prefix

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-/opt/conda/condabin/conda create -y -q --prefix /anaconda/envs --channel=conda-forge --name jupyter_env python=3.8 jupyter_server
+/opt/conda/condabin/conda create -y -q --prefix /anaconda/envs/jupyter_env --channel=conda-forge  python=3.8 jupyter_server
 /anaconda/envs/jupyter_env/bin/pip install \
     azureml-core==1.56.0 \
     azureml-telemetry==1.56.0 \

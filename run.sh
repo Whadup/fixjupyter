801becb574e3c384ef8e0f11a694f683739ceda7
Lukas Pfahler <lukas.pfahler@udo.edu>
2024-09-26 11:30:31 +0000

    tag

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+rm -rf /anaconda/envs/*
 /opt/conda/condabin/conda create -y -q --prefix /anaconda/envs/jupyter_env --channel=conda-forge  python=3.8 jupyter_server
 /anaconda/envs/jupyter_env/bin/pip install \
     azureml-core==1.56.0 \
